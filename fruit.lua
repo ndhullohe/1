3c998ce5d7b4f21e4560bee0bc78367d59a717fa
ndhullohe <ndhullohe@gmail.com>
2025-12-30 18:36:14 -0300

    Update fruit.lua

--- fruit.lua
+++ fruit.lua
@@ -33,7 +33,7 @@ repeat task.wait() until LocalPlayer:FindFirstChild("PlayerGui")
 print("[AUTOEXEC] PlayerGui carregado")
 
 -- Seleção de time (ANTES de esperar character completo)
-task.wait(2)
+task.wait(5)
 print("[AUTOEXEC] Verificando se precisa escolher time...")
 if LocalPlayer.PlayerGui:FindFirstChild("Main (minimal)") then
     print("[AUTOEXEC] Escolhendo time:", getgenv().Team)
