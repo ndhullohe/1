05bcb132d719a72a222550698bba5cfcc59b74a5
ndhullohe <ndhullohe@gmail.com>
2026-01-12 18:59:20 -0300

    Update fruit.lua

--- fruit.lua
+++ fruit.lua
@@ -562,18 +562,13 @@ local function TPReturner()
         )
     end)
     
-    if not success then return end
+    if not success or not Site or not Site.data then return end
     
-    local servers = {}
     for _, v in ipairs(Site.data) do
-        if v.playing < v.maxPlayers then
-            table.insert(servers, v.id)
+        if v.playing < v.maxPlayers and v.id ~= game.JobId then
+            TeleportService:TeleportToPlaceInstance(PlaceID, v.id, LocalPlayer)
+            break
         end
-    end
-    
-    if #servers > 0 then
-        local randomServer = servers[math.random(1, #servers)]
-        TeleportService:TeleportToPlaceInstance(PlaceID, randomServer, LocalPlayer)
     end
 end
 
